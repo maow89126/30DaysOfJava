8e7f13a5b86962a6a6c7c8301ac368bfc32c864b
Mao <maow89126@gmail.com>
2020-04-08 11:54:50 +0200

    nothing

--- MySwewtProgram/src/MySweetProgarm.java
+++ MySwewtProgram/src/MySweetProgarm.java
@@ -1,22 +1,12 @@
 import java.util.Scanner;
+
 public class MySweetProgarm {
 
 	public static void main(String[] args) {
-		/*
-		System.out.println("Hello World");
-		//Scanner in  = new Scanner(System.in);
-		//String s = in.nextLine();
-		//String s = new Stirng ("hey"); invoking the constructor 
-		//System.out.println(s);
-		double d = 2.2;
-		System.out.println(d);
-		*/
+		Scanner in = new Scanner("test.txt");
 		
-		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
-		if (s.equals("Sally"))
-			System.out.println("WOW");
 		in.close();
+		
 	}
 }
 
