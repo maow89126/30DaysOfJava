389fc65d39f52951afc2dc5ff3a3dd3f4262edd8
Mao38965 <55671428+Mao38965@users.noreply.github.com>
2020-04-21 21:52:17 +0200

    Convert queue to stack

--- MySwewtProgram/src/MySweetProgarm.java
+++ MySwewtProgram/src/MySweetProgarm.java
@@ -5,28 +5,22 @@ import java.io.FileNotFoundException;
 public class MySweetProgarm {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		Scanner in = new Scanner(new File("students.txt"));
-		
-		List<String> students = new ArrayList<String>();
-		// ArrayList -> class
-		// List -> Interface
-		
-		//In case of int -> List <Integer>
-		while(in.hasNextLine())
+		/*
+		try
 		{
-			students.add(in.nextLine());
-		}
-		
-		for (int i=0; i< students.size();i++)
+			Scanner in  = new Scanner (new File("test"));
+		}catch(FileNotFoundException e)
 		{
-			System.out.println("name: " + students.get(i));
+			e.printStackTrace();
 		}
-		in.close();
-		
+		*/
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Caleb");
+		names.push("Sue");
+		names.push("Sally");
+		// push is stack
+		System.out.println(names.remove());
+		System.out.println(names.remove());
+		System.out.println(names.remove());
 	}
 }
-
-
-// class -instantiation--> object 
-//		 -instantiation--> object
